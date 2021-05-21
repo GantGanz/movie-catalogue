aa12b52248ca3d268517e8dcf02db511dce87408
GantGanz <anggerelegant7@gmail.com>
2021-05-21 23:31:47 +0700

    konfigurasi drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
