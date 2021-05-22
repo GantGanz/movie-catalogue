ddfcde8d41222a6fda3aa1acb4f6438be1ca4915
GantGanz <anggerelegant7@gmail.com>
2021-05-22 15:29:04 +0700

    melakukan request api

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'e87834b01381ee56bb7643ad6b2e11c8',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
